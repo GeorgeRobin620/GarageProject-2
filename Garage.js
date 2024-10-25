5363c1b93d505135637e8e68490275cf4fb826da
GeorgeRobin620 <georgerobin620@gmail.com>
2024-10-25 12:13:27 -0700

    intial

---
+++ Garage.js
@@ -0,0 +1,22 @@
+function myFunction() {
+    let popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+}
+
+const form = document.querySelector("form");
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    let email = document.getElementById("email");
+    console.log(email.value);
+});
+
+function getValue() {
+    let checkboxes = document.getElementsByName('items');
+    let result = "";
+    for (var i = 0; i < checkboxes.length; i++) {
+        if (checkboxes[i].checked) {
+            result += checkboxes[i].value + " " + " items, ";
+        }
+    }
+    console.log("<p> You have selected : " + result);
+};
